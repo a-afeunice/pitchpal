2251a0a980f2c0dea8ce9958a9c468bb85b4633f
a-afeunice <afrifae2@gmail.com>
2024-08-02 10:59:55 +0000

    added path configuration

---
+++ src/layouts/rootLayout.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/footer";
+
+const RootLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default RootLayout;
---
+++ src/pages/landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <>
+     <div>hero</div>
+     <div>about</div>
+    </>
+   
+    
+  )
+}
+
+export default Landing
